28dab3844420a60adbaa1a3abc0f25bf4751fc3d
Wrik Das <wrikdas22@gmail.com>
2020-11-15 16:47:46 +0530

    added appname to the notification

--- Scripts/emojipicker.sh
+++ Scripts/emojipicker.sh
@@ -4,4 +4,4 @@
 fzf < ~/Scripts/emojipicker-assets/emoji_list.txt | awk '{print $1}' | tr -d \\n | xsel --clipboard
 
 # send notification
-notify-send --urgency=low --expire-time=3000 "$(xsel --clipboard --output) copied to clipboard"
+notify-send --app-name="EmojiPicker" --urgency=low --expire-time=3000 "$(xsel --clipboard --output) copied to clipboard"
