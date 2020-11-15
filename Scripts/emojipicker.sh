e3c7ed623c21aec38271a1a96098b478bfc2438d
Wrik Das <wrikdas22@gmail.com>
2020-11-15 15:50:02 +0530

    show notification copied to clipboard

--- Scripts/emojipicker.sh
+++ Scripts/emojipicker.sh
@@ -1,2 +1,7 @@
 #/usr/bin/zsh
+
+# get emoji, remove newline, copy to clipboard
 fzf < ~/Scripts/emojipicker-assets/emoji_list.txt | awk '{print $1}' | tr -d \\n | xsel --clipboard
+
+# send notification
+notify-send --urgency=low --expire-time=3000 "$(xsel --clipboard --output) copied to clipboard"
